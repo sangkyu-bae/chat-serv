68ff7dd9f9cb14845bf8a980d74456738db418b9
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2025-09-18 21:36:09 +0900

    채팅 팬아웃 소비 수정

--- chat-server-kotiln/src/main/kotlin/com/chat/chatserverkotiln/domain/chat/service/ChatMessageService.kt
+++ chat-server-kotiln/src/main/kotlin/com/chat/chatserverkotiln/domain/chat/service/ChatMessageService.kt
@@ -5,18 +5,23 @@ import com.chat.chatserverkotiln.module.websocket.WebSocketSessionManager
 import jakarta.annotation.PostConstruct
 import jakarta.annotation.PreDestroy
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import reactor.core.Disposable
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Component
 class ChatMessageService(
     private val subscriber: ChatSubscriber,
     private val redisRepository: RedisRepository,
-    private val webSocketSessionManager: WebSocketSessionManager
+    private val webSocketSessionManager: WebSocketSessionManager,
+    @Value("\${chat.server-id}") private val serverId : String
 ) {
 
     companion object {
         private const val ROOM_KEY_PREFIX = "chat:room:join-user"
+        private const val SESSION_KEY_PREFIX = "connected:session:"
         private const val SERVER_SEND_CONCURRENCY = 8
     }
 
@@ -25,18 +30,27 @@ class ChatMessageService(
 
     @PostConstruct
     fun start() {
+        val serverKey = "$SESSION_KEY_PREFIX$serverId"
+
         // 무한 구독 시작
         subscriber.subscribe()
             .onBackpressureBuffer(10_000)
             .flatMap({ msg ->
-                val key = "$ROOM_KEY_PREFIX:${msg.roomId}"
-                log.info("key : {}",key);
-                redisRepository.getAllSetMembers(key)      // Flux<String>
-                    .flatMap({ userId ->
+                val roomKey = "$ROOM_KEY_PREFIX:${msg.roomId}"
 
-                        webSocketSessionManager.sendToUser(userId, msg.content) // Mono<Void>
-                    }, 32)
-                    .then() // Mono<Void>
+                // 서버 유저만 먼저 Mono<List<String>>로 조회
+                redisRepository.getAllSetMembers(serverKey)
+                    .collectList()
+                    .flatMapMany { serverUsers ->
+                        val serverSet = serverUsers.toSet()
+
+                        // roomUsers는 collect 안 하고 바로 streaming
+                        redisRepository.getAllSetMembers(roomKey)
+                            .filter { userId -> serverSet.contains(userId) }
+                            .flatMap({ userId ->
+                                webSocketSessionManager.sendToUser(userId, msg.content)
+                            }, 32)
+                    }
             }, 64)
             .onErrorContinue { e, v -> log.warn("fanout 실패: value=$v, err=${e.message}") }
             .subscribe()
