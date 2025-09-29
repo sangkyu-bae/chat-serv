e365098d797e3bbdc4e20c66ddfaa9386bfc38d2
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2025-09-30 00:45:30 +0900

    유저별 채팅방 정보

---
+++ proxy-kotlin/src/test/kotlin/com/chat/proxykotlin/ProxyKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chat.proxykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProxyKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
